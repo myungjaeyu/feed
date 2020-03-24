64f0224a61e0d192d9cd84531437b4ce03c9c870
u4bi <rootcode10@gmail.com>
2020-03-25 00:56:37 +0900

    feat(client): 디폴트 레이아웃 내 PageTemplate 모듈 적용

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
 
+import { HeaderTitle, HeaderTab, PageTemplate } from 'design-system'
+
 interface ILayoutProps {
     children: any
 }
@@ -7,7 +9,12 @@ interface ILayoutProps {
 const Layout = ({ children }: ILayoutProps) => {
     return (
         <Fragment>
-            {children}
+            <PageTemplate
+                HeaderTitle={<HeaderTitle title='오늘 뭐입지?' description='18. 11. 28. 13:00' />}
+                HeaderTab={<HeaderTab tabs={[<button className={'selected'}>상품</button>, <button>랭킹</button>]} />}
+            >
+                {children}
+            </PageTemplate>
         </Fragment>
     )
 }
