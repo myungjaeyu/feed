565db3b629932e0be0bd8904c1dd796a8085c5bd
u4bi <rootcode10@gmail.com>
2020-03-24 23:07:04 +0900

    chore(design-system): 불필요한 리소스 제거

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,6 +1,3 @@
 import { Button } from './atoms/Button'
-import { Progress } from './atoms/Progress'
-import { ButtonGroup } from './molecules/ButtonGroup'
-import { Carousel, CarouselItem } from './organisms/Carousel'
 
-export { Button, Progress, ButtonGroup, Carousel, CarouselItem }
+export { Button }
