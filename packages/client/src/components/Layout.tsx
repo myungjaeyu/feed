76f7c46d64023ff254b9970d9f7b919ec5df786c
u4bi <rootcode10@gmail.com>
2020-03-25 04:19:36 +0900

    feat(client): pathname을 통해 선택중인 HeaderTab 하이라이트 표시

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { useLocation } from 'react-use'
 
 import { HeaderTitle, HeaderTab, PageTemplate } from 'design-system'
 
@@ -7,11 +8,21 @@ interface ILayoutProps {
 }
 
 const Layout = ({ children }: ILayoutProps) => {
+
+    const location = useLocation()
+    const isSelectedTab = pathname => location.pathname === pathname
+    const attachSelectedClassName = bool => bool && 'selected' || ''
+
     return (
         <Fragment>
             <PageTemplate
                 HeaderTitle={<HeaderTitle title='오늘 뭐입지?' description='18. 11. 28. 13:00' />}
-                HeaderTab={<HeaderTab tabs={[<button className={'selected'}>상품</button>, <button>랭킹</button>]} />}
+                HeaderTab={<HeaderTab 
+                    tabs={[
+                        <button key={'1'} className={ attachSelectedClassName(isSelectedTab('/')) }>상품</button>, 
+                        <button key={'2'} className={ attachSelectedClassName(isSelectedTab('/ranking')) }>랭킹</button>
+                    ]} 
+                />}
             >
                 {children}
             </PageTemplate>
