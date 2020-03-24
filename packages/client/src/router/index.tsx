90b7159a1092597ef91d0d00bf2d6f58da2ff56f
u4bi <rootcode10@gmail.com>
2020-03-24 23:02:12 +0900

    chore(client): 불필요한 리소스 제거

--- packages/client/src/router/index.tsx
+++ packages/client/src/router/index.tsx
@@ -7,7 +7,6 @@ import {
     Switch
 } from 'react-router-dom'
 
-const Login = lazy(() => import('@pages/login'))
 const Home = lazy(() => import('@pages/home'))
 const NotFound = lazy(() => import('@pages/not-found'))
 
@@ -18,7 +17,6 @@ const Router = ({}: IRouterProps) => {
         <DefaultRouter>
             <Suspense fallback={<div />}>
                 <Switch>
-                    <Route path='/login' exact component={Login} />
                     <Route path='/' exact component={Home} />
                     <Route component={NotFound} />
                 </Switch>
