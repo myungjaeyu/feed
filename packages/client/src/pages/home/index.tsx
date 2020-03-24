f217141e283c0b095fe49a6ac7fd63b8ec822df3
u4bi <rootcode10@gmail.com>
2020-03-25 05:20:37 +0900

    feat(client): 상품 카드 클릭 시 해당 쇼핑몰 상품으로 이동하는 네비게이션 적용

--- packages/client/src/pages/home/index.tsx
+++ packages/client/src/pages/home/index.tsx
@@ -4,6 +4,8 @@ import Layout from '@components/Layout'
 
 import { Tile, TilesTemplate } from 'design-system'
 
+import Link from '@components/Link'
+
 interface IHomeProps {}
 
 const Home = ({}: IHomeProps) => {
@@ -12,13 +14,27 @@ const Home = ({}: IHomeProps) => {
         <Layout>
 
             <TilesTemplate>
-                <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
-                <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
-                <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
-                <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
-                <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
-                <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
-                <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fjtamtam.co.kr%2Fproduct%2Fdetail.html%3Fproduct_no%3D5393%26cate_no%3D4%26display_group%3D1%26utm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fjtamtam.co.kr%2Fproduct%2Fdetail.html%3Fproduct_no%3D5393%26cate_no%3D4%26display_group%3D1%26utm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fjtamtam.co.kr%2Fproduct%2Fdetail.html%3Fproduct_no%3D5393%26cate_no%3D4%26display_group%3D1%26utm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fjtamtam.co.kr%2Fproduct%2Fdetail.html%3Fproduct_no%3D5393%26cate_no%3D4%26display_group%3D1%26utm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fjtamtam.co.kr%2Fproduct%2Fdetail.html%3Fproduct_no%3D5393%26cate_no%3D4%26display_group%3D1%26utm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fjtamtam.co.kr%2Fproduct%2Fdetail.html%3Fproduct_no%3D5393%26cate_no%3D4%26display_group%3D1%26utm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fjtamtam.co.kr%2Fproduct%2Fdetail.html%3Fproduct_no%3D5393%26cate_no%3D4%26display_group%3D1%26utm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Tile image='https://usercontents-c.styleshare.kr/images/41316547/original' price='15,000원' />
+                </Link>
             </TilesTemplate>
 
         </Layout>
