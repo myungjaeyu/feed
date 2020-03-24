e08273e63c441a84f1320b636c664572634e4982
u4bi <rootcode10@gmail.com>
2020-03-25 05:21:21 +0900

    feat(client): 쇼핑몰 클릭 시 해당 쇼핑몰로 이동하는 네비게이션 적용

--- packages/client/src/pages/ranking/index.tsx
+++ packages/client/src/pages/ranking/index.tsx
@@ -4,6 +4,8 @@ import Layout from '@components/Layout'
 
 import { Rank, ListTemplate } from 'design-system'
 
+import Link from '@components/Link'
+
 interface IRankProps {}
 
 const Ranking = ({}: IRankProps) => {
@@ -13,13 +15,27 @@ const Ranking = ({}: IRankProps) => {
 
             <ListTemplate>
 
-                <Rank rank={1} title='소녀나라' description='10대, 캐쥬얼' />
-                <Rank rank={2} title='소녀나라' description='10대, 캐쥬얼' />
-                <Rank rank={3} title='소녀나라' description='10대, 캐쥬얼' />
-                <Rank rank={4} title='소녀나라' description='10대, 캐쥬얼' />
-                <Rank rank={5} title='소녀나라' description='10대, 캐쥬얼' />
-                <Rank rank={6} title='소녀나라' description='10대, 캐쥬얼' />
-                <Rank rank={7} title='소녀나라' description='10대, 캐쥬얼' />
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fwww.gosister.co.kr%3Futm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Rank rank={1} title='소녀나라' description='10대, 캐쥬얼' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fwww.gosister.co.kr%3Futm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Rank rank={2} title='소녀나라' description='10대, 캐쥬얼' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fwww.gosister.co.kr%3Futm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Rank rank={3} title='소녀나라' description='10대, 캐쥬얼' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fwww.gosister.co.kr%3Futm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Rank rank={4} title='소녀나라' description='10대, 캐쥬얼' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fwww.gosister.co.kr%3Futm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Rank rank={5} title='소녀나라' description='10대, 캐쥬얼' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fwww.gosister.co.kr%3Futm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Rank rank={6} title='소녀나라' description='10대, 캐쥬얼' />
+                </Link>
+                <Link href='https://www.stylesha.re/+webview/soho/redirect?region=kr&to=http%3A%2F%2Fwww.gosister.co.kr%3Futm_source%3Dstyleshare%26cafe_mkt%3Dstyleshare' target='_blank'>
+                    <Rank rank={7} title='소녀나라' description='10대, 캐쥬얼' />
+                </Link>
 
             </ListTemplate>
 
