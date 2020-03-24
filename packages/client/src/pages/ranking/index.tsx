a35cfe0afd1a7142752a90a0ba3ff7a029000869
u4bi <rootcode10@gmail.com>
2020-03-25 03:52:52 +0900

    feat(client): Ranking 페이지 내 RankListTemplate 모듈 적용

--- packages/client/src/pages/ranking/index.tsx
+++ packages/client/src/pages/ranking/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Layout from '@components/Layout'
 
+import { Rank, ListTemplate } from 'design-system'
+
 interface IRankProps {}
 
 const Ranking = ({}: IRankProps) => {
@@ -9,9 +11,17 @@ const Ranking = ({}: IRankProps) => {
     return (
         <Layout>
 
-            <h1>
-                Ranking
-            </h1>
+            <ListTemplate>
+
+                <Rank rank={1} title='소녀나라' description='10대, 캐쥬얼' />
+                <Rank rank={2} title='소녀나라' description='10대, 캐쥬얼' />
+                <Rank rank={3} title='소녀나라' description='10대, 캐쥬얼' />
+                <Rank rank={4} title='소녀나라' description='10대, 캐쥬얼' />
+                <Rank rank={5} title='소녀나라' description='10대, 캐쥬얼' />
+                <Rank rank={6} title='소녀나라' description='10대, 캐쥬얼' />
+                <Rank rank={7} title='소녀나라' description='10대, 캐쥬얼' />
+
+            </ListTemplate>
 
         </Layout>
     )
